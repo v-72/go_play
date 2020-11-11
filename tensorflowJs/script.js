26ae28d542e2fd18a874008622c62a5a366082f6
Vinay <vinayhegde@outlook.com>
2020-11-11 09:57:18 +0530

    Added training model

--- tensorflowJs/script.js
+++ tensorflowJs/script.js
@@ -1,39 +1,112 @@
 async function getData() {
-    const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
-    const carsData = await carsDataResponse.json();  
+    const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+    const carsData = await carsDataResponse.json();
     const cleaned = carsData.map(car => ({
-      mpg: car.Miles_per_Gallon,
-      horsepower: car.Horsepower,
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
     }))
-    .filter(car => (car.mpg != null && car.horsepower != null));
-    
-    return cleaned;
-  }
+        .filter(car => (car.mpg != null && car.horsepower != null));
 
-  async function run() {
+    return cleaned;
+}
+
+async function run() {
     const data = await getData();
     const values = data.map(d => ({
-      x: d.horsepower,
-      y: d.mpg,
+        x: d.horsepower,
+        y: d.mpg,
     }));
-  
+
     tfvis.render.scatterplot(
-      {name: 'Horsepower v MPG'},
-      {values}, 
-      {
-        xLabel: 'Horsepower',
-        yLabel: 'MPG',
-        height: 300
-      }
+        { name: 'Horsepower v MPG' },
+        { values },
+        {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 300
+        }
     );
-  }
-  
-  function createModel() {
-    const model = tf.sequential(); 
-    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
-    model.add(tf.layers.dense({units: 1, useBias: true}));
-  
+}
+
+function createModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
+    model.add(tf.layers.dense({ units: 1, useBias: true }));
+
     return model;
+}
+
+function convertToTensor(data) {
+    return tf.tidy(() => {
+        tf.util.shuffle(data);
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg);
+
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            inputMax,
+            inputMin,
+            labelMax,
+            labelMin,
+        }
+    });
+}
+tf.util.shuffle(data);
+const inputs = data.map(d => d.horsepower)
+const labels = data.map(d => d.mpg);
+
+const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+const inputMax = inputTensor.max();
+const inputMin = inputTensor.min();
+const labelMax = labelTensor.max();
+const labelMin = labelTensor.min();
+
+const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+return {
+    inputs: normalizedInputs,
+    labels: normalizedLabels,
+    // Return the min/max bounds so we can use them later.
+    inputMax,
+    inputMin,
+    labelMax,
+    labelMin,
+}
+
+async function trainModel(model, inputs, labels) {
+    // Prepare the model for training.  
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: tf.losses.meanSquaredError,
+      metrics: ['mse'],
+    });
+    
+    const batchSize = 32;
+    const epochs = 50;
+    
+    return await model.fit(inputs, labels, {
+      batchSize,
+      epochs,
+      shuffle: true,
+      callbacks: tfvis.show.fitCallbacks(
+        { name: 'Training Performance' },
+        ['loss', 'mse'], 
+        { height: 200, callbacks: ['onEpochEnd'] }
+      )
+    });
   }
 
-  document.addEventListener('DOMContentLoaded', run);
+document.addEventListener('DOMContentLoaded', run);
