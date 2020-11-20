f8ecedc701f567bc46d87d63cdb7e140721059e1
Vinay <vinayhegde@outlook.com>
2020-11-20 10:17:39 +0530

    Changes in socket boilerplate

---
+++ algo/candy_crush.js
@@ -0,0 +1,11 @@
+function hourglassSum(arr) {
+    let maxVal = -1;
+    for(let i =0; i<arr.length -2;i++){
+        for(let j=0; j<arr[i].length-2;j++){
+            let currentVal = arr[i][j]+arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            maxVal = Math.max(maxVal,currentVal);
+        }
+    }
+    return maxVal;
+}
+
---
+++ go/socket_boilerplate.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+)
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	socket net.Conn
+	data   chan []byte
+}
+
+func startServerMode() {}
+
+func startClientMode() {}
+
+func main() {
+	fmt.Println("Socket creation ")
+	flagMode := flag.String("mode", "server", "start in client or server mode")
+	flag.Parse()
+	if strings.ToLower(*flagMode) == "server" {
+		startServerMode()
+	} else {
+		startClientMode()
+	}
+}
